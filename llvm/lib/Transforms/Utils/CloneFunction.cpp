6065ae1d7ad0609c8a324321d81fdc8345cfe17c
Jeroen Dobbelaere <jeroen.dobbelaere@synopsys.com>
2023-04-28 23:12:38 +0200

    Use noalias intrinsics when inlining and keep metadata up to date. (Based on D9400)
    Produce unique noalias scopes at the location of the function call.
    Connect llvm.noalias.decl to other noalias intrinsics, now that the
    declaration and the usage see each other.
    
    [noalias] D9400: llvm.noalias - Use noalias intrinsics when inlining (and update them when cloning metadata)
    
    Rebase of https://reviews.llvm.org/D9400

--- llvm/lib/Transforms/Utils/CloneFunction.cpp
+++ llvm/lib/Transforms/Utils/CloneFunction.cpp
@@ -108,6 +108,21 @@ createIdentityMDPredicate(const Function &F, CloneFunctionChangeType Changes) {
   };
 }
 
+static void PropagateNoAliasProvenanceInfo(Instruction *To,
+                                           const Instruction *From) {
+  // The ptr_provenance is not automatically copied over in a 'clone()'
+  // Let's do it here.
+  if (auto *LI = dyn_cast<LoadInst>(From)) {
+    if (LI->hasPtrProvenanceOperand())
+      cast<LoadInst>(To)->setPtrProvenanceOperand(
+          LI->getPtrProvenanceOperand());
+  } else if (auto SI = dyn_cast<StoreInst>(From)) {
+    if (SI->hasPtrProvenanceOperand())
+      cast<StoreInst>(To)->setPtrProvenanceOperand(
+          SI->getPtrProvenanceOperand());
+  }
+}
+
 /// See comments in Cloning.h.
 BasicBlock *llvm::CloneBasicBlock(const BasicBlock *BB, ValueToValueMapTy &VMap,
                                   const Twine &NameSuffix, Function *F,
@@ -121,6 +136,8 @@ BasicBlock *llvm::CloneBasicBlock(const BasicBlock *BB, ValueToValueMapTy &VMap,
   // Loop over all instructions, and copy them over.
   for (const Instruction &I : *BB) {
     Instruction *NewInst = I.clone();
+    PropagateNoAliasProvenanceInfo(NewInst, &I);
+
     if (I.hasName())
       NewInst->setName(I.getName() + NameSuffix);
 
@@ -564,6 +581,7 @@ void PruningFunctionCloner::CloneBlock(
 
     Instruction *NewInst = cloneInstruction(II);
     NewInst->insertInto(NewBB, NewBB->end());
+    PropagateNoAliasProvenanceInfo(NewInst, &*II);
 
     if (HostFuncIsStrictFP) {
       // All function calls in the inlined function must get 'strictfp'
@@ -1126,6 +1144,7 @@ BasicBlock *llvm::DuplicateInstructionsInSplitBetween(
   // terminator gets replaced and StopAt == BB's terminator.
   for (; StopAt != &*BI && BB->getTerminator() != &*BI; ++BI) {
     Instruction *New = BI->clone();
+    PropagateNoAliasProvenanceInfo(New, &*BI);
     New->setName(BI->getName());
     New->insertBefore(NewTerm->getIterator());
     New->cloneDebugInfoFrom(&*BI);
